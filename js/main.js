8213e70b9b036b79b00cb02ad25409cf46d143bb
Juliyapz <76488867+Juliyapz@users.noreply.github.com>
2022-02-14 23:59:22 +0900

    day 4 part 2

--- js/main.js
+++ js/main.js
@@ -25,9 +25,24 @@ let menu = document.querySelector('.header');
 burger.addEventListener('click', () => {
   menu.style.display = 'block';
   close.style.display = 'block';
-})
+});
 
 close.addEventListener('click', () => {
   menu.style.display = 'none';
   close.style.display = 'none';
+});
+
+let modalWindow = document.querySelector(".modal");
+let buttonModal = document.querySelector(".main__button-play");
+
+buttonModal.addEventListener('click', () => {
+  modalWindow.classList.add("activ");
+})
+
+modalWindow.addEventListener('click', (e) => {
+  const inModal = e.target.closest('.modal__inner');
+
+  if (!inModal) {
+    modalWindow.classList.remove("activ");
+  }
 })
