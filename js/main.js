7b83997d632a24803779984387cd759059233429
Juliyapz <76488867+Juliyapz@users.noreply.github.com>
2022-02-14 19:34:00 +0900

    day 4

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 const swiper = new Swiper('.main-slider', {
-  slidesPerView: 1,
   spaceBetween: 28.43,
   loop: true,
 
   breakpoints: {
-    376: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    768: {
       slidesPerView: 2,
     }
   },
+
 
   navigation: {
     nextEl: '.main-slider__arrow',
   },
 });
+
+let close = document.querySelector('.header__menu-close');
+let burger = document.querySelector('.menu-burger');
+let menu = document.querySelector('.header');
+
+burger.addEventListener('click', () => {
+  menu.style.display = 'block';
+  close.style.display = 'block';
+})
+
+close.addEventListener('click', () => {
+  menu.style.display = 'none';
+  close.style.display = 'none';
+})
